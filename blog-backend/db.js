650cd9cc9959c77ab84dcff5cea68f13ac2e0b69
Mohd-Murtaza <mohd.murtaza0101@gmail.com>
2024-08-13 06:29:40 +0530

    Remove unwanted folder

--- blog-backend/db.js
+++
@@ -1,4 +0,0 @@
-require("dotenv").config();
-const mongoose=require("mongoose");
-const connection=mongoose.connect(process.env.MONGO_URI)
-module.exports={connection};
--- blog-backend/models/blacklistModel.js
+++
@@ -1,6 +0,0 @@
-const mongoose=require("mongoose");
-const blacklistSchema=mongoose.Schema({
-    accessToken:{type:String,required:true}
-},{versionKey:false});
-const BlacklistModel=mongoose.model("blacklistToken",blacklistSchema);
-module.exports={BlacklistModel};
